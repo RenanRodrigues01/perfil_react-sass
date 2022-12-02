8c256ea95b33c97762979ca065d12ce7015ba127
renan <renan.rodriguesmoura2022@gmail.com>
2022-12-01 22:52:24 -0300

    adicionando links

--- src/componets/SocialNetworks.jsx
+++ src/componets/SocialNetworks.jsx
@@ -3,16 +3,16 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa"
 import "../styles/componets/SocialNetworks.sass";
 
 const socialNetworks = [
-    {name: "linkedin", icon: <FaLinkedinIn />},
-    {name: "github", icon: <FaGithub />},
-    {name: "instagram", icon: <FaInstagram />},
+    {name: "linkedin", link: "https://www.linkedin.com/in/renanrodrigues01/", icon: <FaLinkedinIn />},
+    {name: "github", link: "https://github.com/RenanRodrigues01", icon: <FaGithub />},
+    {name: "instagram", link: "https://www.instagram.com/renan.rodrigues_moura/", icon: <FaInstagram />},
 ];
 
 const SocialNetworks = () => {
   return (
    <section id="social-networks">
     {socialNetworks.map((network) => (
-        <a href="#" className="social-btn" id={network.name} key={network.name}>
+        <a href={network.link} className="social-btn" id={network.name} key={network.name}>
             {network.icon}
         </a>
     ))}
