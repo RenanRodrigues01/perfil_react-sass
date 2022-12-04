f32f7cb9707a64d6219ce00ba6e3be705f8bdfb2
renan <renan.rodriguesmoura2022@gmail.com>
2022-12-03 21:47:21 -0300

    Finalizando projetos

--- src/componets/ProjectsContainer.jsx
+++ src/componets/ProjectsContainer.jsx
@@ -1,14 +1,35 @@
 import ClimaTempo from '../img/clima_tempo.gif'
 import Kanban from '../img/kanban.gif'
 import SimpleStore from '../img/simple_store.gif'
-import Cotacao from '../img/cotacao_moeda.png'
+import TodoList from '../img/to-do_list.gif'
 
 import '../styles/componets/ProjectsContainer.sass'
 
 const projects =[
-  {id: "1", name: "clima Tempo", icon: ClimaTempo, description: "Projeto de consumo de api"},
-  {id: "2", name: "kanban", icon: Kanban},
-  {id: "3", name: "Simple store", icon: SimpleStore},
+  { id: "1", 
+    name: "Clima Tempo", 
+    icon: ClimaTempo, 
+    description: "Projeto feito para o aperfeiçoamento no consumo de APIs, e no trabalho com promisses junto das funções assíncronas.",
+    to: "https://github.com/RenanRodrigues01/clima_tempo" 
+  },
+  { id: "2", 
+    name: "Kanban", 
+    icon: Kanban,
+    description: "Projeto feito com intuito de melhorar o uso dos componentes e Props, trabalhando com a desitruturação da mesma.",
+    to: "https://github.com/RenanRodrigues01/react-kanban"
+  },
+  { id: "3", 
+    name: "Simple Store", 
+    icon: SimpleStore,
+    description:"Projeto feito com HTML, CSS e JavaScript, com a função de melhorar minhas habilidades na criação de layout.",
+    to: "https://github.com/RenanRodrigues01/Simple_Store"
+  },
+  { id: "4", 
+    name: "To-do List", 
+    icon: TodoList,
+    description:"CRUD feito com JavaScript Vanilla e com o auxílio do LocalStorage para o armazenamento das tarefas, trazendo uma melhor experiência para o usuário.",
+    to: "https://github.com/RenanRodrigues01/to-do-list"
+  }
 ];
 
 const ProjectsContainer = () => {
@@ -19,10 +40,11 @@ const ProjectsContainer = () => {
       <div className="project-grid">
         {projects.map((project) => (
           <div className="project-card" key={project.id}>
+            <a href={project.to} target="_blank">
             <img src={project.icon} alt={project.name} />
             <h3>{project.name}</h3>
-            <p></p>
-            <a href="#" className="btn">Saiba mais</a>
+            <p>{project.description}</p>
+            </a>
           </div>
         ))}
       </div>
