7b8cf0cf2c343ee3500e17ac8bd7a1902d7f60e8
renan <renan.rodriguesmoura2022@gmail.com>
2022-12-04 22:32:31 -0300

    adicionando sobre

--- src/componets/AboutContainer.jsx
+++ src/componets/AboutContainer.jsx
@@ -4,9 +4,8 @@ const AboutContainer = () => {
   return (
   <section className="about-container">
     <h2>Sobre</h2>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ipsam dicta sint, provident blanditiis repellat, laborum minus voluptatibus quaerat at eius. Placeat ut repellendus unde expedita consectetur suscipit exercitationem nisi.</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse at libero quas obcaecati molestiae autem temporibus quisquam optio corporis placeat soluta atque qui maiores eveniet ducimus, possimus animi cupiditate necessitatibus.</p>
-    
+    <p>Olá, me chamo Renan Rodrigues de Moura, e após quase 10 anos trabalhando com atendimento ao público, resolvi dar um passo à frente e correr atrás do que sempre gostei: tecnologia.
+      Atualmente sou um desenvolvedor focado no Front-end, cursando análise e desenvolvimento de sistemas. Procuro sempre desenvolver projetos desafiadores que me tirem da zona de conforto, assim me proporcionando um maior aprendizado, para conseguir evoluir constantemente.</p> 
   </section>
   )
 }
